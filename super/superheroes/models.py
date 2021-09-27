5451b43630b1274bd9f92ba1ac9f62b007ea8cf1
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-09-26 23:32:30 -0700

    "fixed bugs for update"

--- super/superheroes/models.py
+++ super/superheroes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Superhero(models.Model):
     id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=50)
